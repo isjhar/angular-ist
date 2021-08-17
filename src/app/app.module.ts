cfcb9d63d49cb40a31c229560bd16af395063ff7
Isjhar Kautsar <isjhar@gmail.com>
2021-08-17 18:02:19 +0700

    redirect user when unauthorized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor, authInterceptorProviders } from './auth.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/auth.interceptor.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next
+      .handle(request)
+      .pipe(catchError((x) => this.handleAuthError(x)));
+  }
+
+  private handleAuthError(err: HttpErrorResponse): Observable<any> {
+    //handle your auth error or rethrow
+    if (
+      this.router.url !== '/login' &&
+      (err.status === 401 || err.status === 403)
+    ) {
+      //navigate /delete cookies or whatever
+      this.router.navigateByUrl(`/login`);
+      // if you've caught / handled the error, you don't want to rethrow it unless you also want downstream consumers to have to handle it as well.
+      return of(err.message); // or EMPTY may be appropriate here
+    }
+    return throwError(err);
+  }
+}
+
+export const authInterceptorProviders = [
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true,
+  },
+];
