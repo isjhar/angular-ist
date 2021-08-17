8e84c594ef085e9ca8dde622364ade9410f73c0e
Isjhar Kautsar <isjhar@gmail.com>
2021-08-17 14:17:24 +0700

    add pretier

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,28 +6,28 @@ import { UserManagementService } from '../user-management.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   loginForm = new FormGroup({
     username: new FormControl(''),
     password: new FormControl(''),
-  })
+  });
 
-  constructor(private usermanagementService: UserManagementService, private router: Router) { }
+  constructor(
+    private usermanagementService: UserManagementService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   onLoginformSubmited(): void {
-    this.usermanagementService.getCsrfToken()
-    .subscribe(response => {
+    this.usermanagementService.getCsrfToken().subscribe((response) => {
       console.log(response);
-      this.usermanagementService.login(this.loginForm.value)
-        .subscribe(response => {
-          this.router.navigate(['dashboard'])
+      this.usermanagementService
+        .login(this.loginForm.value)
+        .subscribe((response) => {
+          this.router.navigate(['dashboard']);
         });
     });
   }
