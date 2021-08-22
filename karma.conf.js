d43217acf2db228ab563e5131b27f6948de2823d
Isjhar Kautsar <isjhar@gmail.com>
2021-08-22 21:21:00 +0700

    rename project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/murajaah-assistant-web'),
+      dir: require('path').join(__dirname, './coverage/smirs-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
