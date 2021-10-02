0e8fc9dff6cdf6a8f24f40e515438cf3022a6120
Isjhar Kautsar <isjhar@gmail.com>
2021-10-02 16:00:26 +0700

    fix status code unauthorized

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -27,10 +27,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
   private handleAuthError(err: HttpErrorResponse): Observable<any> {
     //handle your auth error or rethrow
-    if (
-      this.router.url !== '/login' &&
-      (err.status === 401 || err.status === 403 || err.status == 419)
-    ) {
+    if (this.router.url !== '/login' && err.status === 401) {
       this.authService.logout();
       this.router.navigateByUrl(`/login`);
       // if you've caught / handled the error, you don't want to rethrow it unless you also want downstream consumers to have to handle it as well.
