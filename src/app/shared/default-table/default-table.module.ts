58ba5748c7db9137b4d636b4cf0c8aafed1a6487
Isjhar Kautsar <isjhar@gmail.com>
2022-04-23 13:25:11 +0700

    add search field

--- src/app/shared/default-table/default-table.module.ts
+++ src/app/shared/default-table/default-table.module.ts
@@ -5,10 +5,25 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { ServerSideTableComponent } from './server-side-table/server-side-table.component';
 import { MatSortModule } from '@angular/material/sort';
+import { SearchFieldComponent } from './server-side-table/search-field/search-field.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [DefaultTableComponent, ServerSideTableComponent],
+  declarations: [
+    DefaultTableComponent,
+    ServerSideTableComponent,
+    SearchFieldComponent,
+  ],
   exports: [DefaultTableComponent, ServerSideTableComponent],
-  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+  ],
 })
 export class DefaultTableModule {}
