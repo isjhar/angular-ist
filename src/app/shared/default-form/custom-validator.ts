d5d523ba712366d46b7d6f90b7179b7b0f6183a6
Isjhar Kautsar <isjhar@gmail.com>
2022-05-07 21:35:40 +0700

    fix custom validator

--- src/app/shared/default-form/custom-validator.ts
+++ src/app/shared/default-form/custom-validator.ts
@@ -1,13 +1,12 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export class CustomValidator {
-  static timeFormat(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      let input: string = control.value;
-      let found = input.match(/(2[0-3]|[0-1][0-9]):([0-5][0-9])/g);
-      return !found || found.length == 0
-        ? { timeFormat: { value: control.value } }
-        : null;
-    };
+  static timeFormat(control: AbstractControl): ValidationErrors | null {
+    let input: string = control.value;
+    if (!input) return null;
+    let found = input.match(/^(2[0-3]|[0-1][0-9]):([0-5][0-9])$/g);
+    return !found || found.length == 0
+      ? { timeFormat: { value: control.value } }
+      : null;
   }
 }
