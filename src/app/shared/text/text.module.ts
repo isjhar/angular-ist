c543665b37460d408b06c685a858d2cc0cb231e5
Isjhar Kautsar <isjhar@gmail.com>
2022-05-09 14:55:57 +0700

    perbaikan text module

--- src/app/shared/text/text.module.ts
+++ src/app/shared/text/text.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultDatePipe } from './default-date.pipe';
 
-
-
 @NgModule({
-  declarations: [
-    DefaultDatePipe
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [DefaultDatePipe],
+  exports: [DefaultDatePipe],
+  imports: [CommonModule],
 })
-export class TextModule { }
+export class TextModule {}
