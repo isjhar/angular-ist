71b3c12bd383fcf81dbcf496807cd06284fb8b51
Isjhar Kautsar <isjhar@gmail.com>
2022-09-17 19:20:57 +0700

    number

--- src/app/shared/text/text.module.ts
+++ src/app/shared/text/text.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultDatePipe } from './default-date.pipe';
 import { DefaultCurrencyPipe } from './default-currency.pipe';
+import { DefaultNumberPipe } from './default-number.pipe';
 
 @NgModule({
-  declarations: [DefaultDatePipe, DefaultCurrencyPipe],
-  exports: [DefaultDatePipe, DefaultCurrencyPipe],
+  declarations: [DefaultDatePipe, DefaultCurrencyPipe, DefaultNumberPipe],
+  exports: [DefaultDatePipe, DefaultCurrencyPipe, DefaultNumberPipe],
   imports: [CommonModule],
 })
 export class TextModule {}
