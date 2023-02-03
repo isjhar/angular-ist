75a73094267b48005110c1f92a7191f8c14d46c0
Isjhar Kautsar <isjhar@gmail.com>
2023-02-03 21:19:15 +0800

    tambah delete

--- src/app/pages/main/setting/access-controls/access-controls.component.ts
+++ src/app/pages/main/setting/access-controls/access-controls.component.ts
@@ -7,6 +7,10 @@ import {
 } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ACCESS_CONTROL_REPOSITORY } from 'src/app/app.module';
+import { DeleteUseCase } from 'src/app/domain/base-use-cases/delete-use-case';
+import { AccessControlRepository } from 'src/app/domain/repositories/access-control-repository';
+import { ConfirmDialogComponent } from 'src/app/pages/shared/confirm-dialog/confirm-dialog.component';
 import { ServerSideTableComponent } from 'src/app/pages/shared/default-table/server-side-table/server-side-table.component';
 import {
   ServerSideTableService,
@@ -36,12 +40,18 @@ export class AccessControlsComponent implements OnInit {
     height: 'auto',
   };
 
+  deleteUseCase: DeleteUseCase;
+
   constructor(
+    @Inject(ACCESS_CONTROL_REPOSITORY)
+    accessControlRepository: AccessControlRepository,
     @Inject(TABLE_SERVICE)
     private tableService: ServerSideTableService<any, any>,
     private dialog: MatDialog,
     private snackBar: MatSnackBar
-  ) {}
+  ) {
+    this.deleteUseCase = new DeleteUseCase(accessControlRepository);
+  }
 
   ngOnInit(): void {
     this.tableService.changeColumns([
@@ -77,6 +87,29 @@ export class AccessControlsComponent implements OnInit {
       if (result) {
         this.table.refreshData();
         this.snackBar.open('Access control added successfully', 'Close', {
+          horizontalPosition: 'start',
+          verticalPosition: 'bottom',
+        });
+      }
+    });
+  }
+
+  onDeleteClicked(element: any): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '30%',
+      maxWidth: 500,
+      height: 'auto ',
+      data: {
+        message: 'Are you sure?',
+        yes$: this.deleteUseCase.execute({
+          id: element.id,
+        }),
+      },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.table.refreshData();
+        this.snackBar.open('Access control deleted successfully', 'Close', {
           horizontalPosition: 'start',
           verticalPosition: 'bottom',
         });
