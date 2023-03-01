87357f5f1d7abfa8630b5b289cf386e9e49db526
Isjhar Kautsar <isjhar@gmail.com>
2023-03-01 16:59:16 +0800

    remove menu from roles

--- src/app/pages/main/setting/roles/add-dialog/add-dialog.component.ts
+++ src/app/pages/main/setting/roles/add-dialog/add-dialog.component.ts
@@ -1,10 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { MENU_REPOSITORY, ROLE_REPOSITORY } from 'src/app/app.module';
-import { MenuRepository } from 'src/app/domain/repositories/menu-repository';
+import { ROLE_REPOSITORY } from 'src/app/app.module';
 import { RoleRepository } from 'src/app/domain/repositories/role-repository';
-import { GetMenusUseCase } from 'src/app/domain/use-cases/get-menus-use-case';
 import { StoreRoleUseCase } from 'src/app/domain/use-cases/store-role-use-case';
 import { UpdateRoleUseCase } from 'src/app/domain/use-cases/update-role-use-case';
 
@@ -34,17 +32,14 @@ export class AddDialogComponent implements OnInit {
     return this.formGroup.get('name') as FormControl;
   }
 
-  getMenusUseCase: GetMenusUseCase;
   storeRoleUseCase: StoreRoleUseCase;
   updateRoleUseCase: UpdateRoleUseCase;
 
   constructor(
-    @Inject(MENU_REPOSITORY) menuRepository: MenuRepository,
     @Inject(ROLE_REPOSITORY) roleRepository: RoleRepository,
     private dialogRef: MatDialogRef<AddDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: AddDialogData
   ) {
-    this.getMenusUseCase = new GetMenusUseCase(menuRepository);
     this.storeRoleUseCase = new StoreRoleUseCase(roleRepository);
     this.updateRoleUseCase = new UpdateRoleUseCase(roleRepository);
   }
