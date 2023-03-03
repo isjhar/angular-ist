f22a867ffcab68a74cbd812c7b615e14fde92a29
Isjhar Kautsar <isjhar@gmail.com>
2023-03-04 05:57:22 +0800

    fix role

--- src/app/data/repositories/api-role-repository.ts
+++ src/app/data/repositories/api-role-repository.ts
@@ -4,15 +4,20 @@ import { map } from 'rxjs/operators';
 import { Pagination } from 'src/app/domain/entities/pagination';
 import { PaginationParams } from 'src/app/domain/entities/pagination-params';
 import { Role } from 'src/app/domain/entities/role';
+import { RoleAccessControl } from 'src/app/domain/entities/role-access-control';
 import {
+  GetRoleAccessControlsRequestParams,
   RoleRepository,
+  StoreAccessControlRequestParams,
   StoreRoleRequestParams,
+  UpdateRoleRequestParams,
 } from 'src/app/domain/repositories/role-repository';
 import { ApiResponse } from '../entities/api-response';
 import { ApiUrlBuilder } from '../utilities/api-url-builder';
 
 export class ApiRoleRepository implements RoleRepository {
   constructor(private http: HttpClient) {}
+
   get(params: PaginationParams): Observable<Pagination<Role>> {
     let urlBuilder = new ApiUrlBuilder('/api/roles');
     urlBuilder.pushQueryParam('page', params.page);
@@ -28,7 +33,7 @@ export class ApiRoleRepository implements RoleRepository {
       .post<ApiResponse<Role>>('/api/roles', params)
       .pipe(map<ApiResponse<Role>, Role>((e) => e.data));
   }
-  update(id: number, params: StoreRoleRequestParams): Observable<void> {
+  update(id: number, params: UpdateRoleRequestParams): Observable<void> {
     return this.http
       .patch<ApiResponse<void>>(`/api/roles/${id}`, params)
       .pipe(map<ApiResponse<void>, void>((e) => e.data));
@@ -37,5 +42,20 @@ export class ApiRoleRepository implements RoleRepository {
     return this.http
       .delete<ApiResponse<void>>(`/api/roles/${id}`)
       .pipe(map<ApiResponse<void>, void>((e) => e.data));
+  }
+  getRoleAccessControls(
+    params: GetRoleAccessControlsRequestParams
+  ): Observable<Pagination<RoleAccessControl>> {
+    throw new Error('Method not implemented.');
+  }
+  storeAccessControl(
+    params: StoreAccessControlRequestParams
+  ): Observable<void> {
+    throw new Error('Method not implemented.');
+  }
+  deleteAccessControl(
+    params: StoreAccessControlRequestParams
+  ): Observable<void> {
+    throw new Error('Method not implemented.');
   }
 }
