5f316f1a8c1b348f920d4802b355921a6e90386c
Isjhar Kautsar <isjhar@gmail.com>
2023-03-04 09:41:58 +0800

    refactor role

--- src/app/pages/main/setting/role.guard.ts
+++ src/app/pages/main/setting/role.guard.ts
@@ -1,14 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 import { ROLE_REPOSITORY } from 'src/app/app.module';
-import { FindUseCase } from 'src/app/domain/base-use-cases/find-use-case';
 import { Role } from 'src/app/domain/entities/role';
 import { RoleRepository } from 'src/app/domain/repositories/role-repository';
 import { BaseDetailGuardGuard } from '../../shared/base-detail-guard.guard';
