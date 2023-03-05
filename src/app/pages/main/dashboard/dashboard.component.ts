037842a4d0f0b37f55ad8f4b67518910a0e8fadb
Isjhar Kautsar <isjhar@gmail.com>
2023-03-05 21:24:43 +0800

    add user dashboard

--- src/app/pages/main/dashboard/dashboard.component.ts
+++ src/app/pages/main/dashboard/dashboard.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AUTHENTICATED_USER_REPOSITORY } from 'src/app/app.module';
+import { AccessControlId } from 'src/app/domain/entities/access-control';
+import { AuthenticatedUserRepository } from 'src/app/domain/repositories/authenticated-user-repository';
+import { HasAuthenticatedUserAccessControlUseCase } from 'src/app/domain/use-cases/has-authenticated-user-access-control-use-case';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  adminAccessControl$: Observable<boolean>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    @Inject(AUTHENTICATED_USER_REPOSITORY)
+    authenticatedUserRepository: AuthenticatedUserRepository
+  ) {
+    let hasAuthenticatedUserAccessControlUseCase =
+      new HasAuthenticatedUserAccessControlUseCase(authenticatedUserRepository);
+    this.adminAccessControl$ = hasAuthenticatedUserAccessControlUseCase.execute(
+      AccessControlId.Setting
+    );
   }
 
+  ngOnInit(): void {}
 }
