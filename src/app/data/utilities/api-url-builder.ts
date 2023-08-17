18a58470144aad724802583677212627511ddf73
Isjhar Kautsar <isjhar@gmail.com>
2023-08-17 16:11:02 +0800

    handle array query param

--- src/app/data/utilities/api-url-builder.ts
+++ src/app/data/utilities/api-url-builder.ts
@@ -5,14 +5,29 @@ export class ApiUrlBuilder {
   }
 
   pushQueryParam(key: string, value?: any) {
-    if (value !== null && value !== undefined && value !== '') {
-      if (!this.urls.find((x) => x == '?')) {
-        this.urls.push('?');
-      } else {
-        this.urls.push('&');
-      }
-      this.urls.push(`${key}=${value}`);
+    if (value === null || value === undefined || value === '') {
+      return;
     }
+
+    if (value.constructor === Array) {
+      for (let index = 0; index < value.length; index++) {
+        const element = value[index];
+        if (!this.urls.find((x) => x == '?')) {
+          this.urls.push('?');
+        } else {
+          this.urls.push('&');
+        }
+        this.urls.push(`${key}[]=${element}`);
+      }
+      return;
+    }
+
+    if (!this.urls.find((x) => x == '?')) {
+      this.urls.push('?');
+    } else {
+      this.urls.push('&');
+    }
+    this.urls.push(`${key}=${value}`);
   }
 
   getUrl(): string {
