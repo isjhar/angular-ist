180c17d7a78038706a9fdd4ecf1361e8ef64de1b
Isjhar Kautsar <isjhar@gmail.com>
2025-08-08 19:11:32 +0800

    logout when unauthorized

--- src/app/api.interceptor.ts
+++ src/app/api.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable, Provider } from '@angular/core';
+import { Inject, Injectable, Provider } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
@@ -9,10 +9,28 @@ import {
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { AUTHENTICATED_USER_REPOSITORY } from 'src/app/app-local-repository';
+import { AuthenticatedUserRepository } from 'src/app/domain/repositories/authenticated-user-repository';
+import { AUTH_REPOSITORY } from 'src/app/app-token-repository';
+import { AuthRepository } from 'src/app/domain/repositories/auth-repository';
+import { LogoutUseCase } from 'src/app/domain/use-cases/logout-use-case';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
-  constructor() {}
+  logoutUseCase: LogoutUseCase;
+
+  constructor(
+    @Inject(AUTHENTICATED_USER_REPOSITORY)
+    authenticatedUserRepository: AuthenticatedUserRepository,
+    @Inject(AUTH_REPOSITORY) authRepository: AuthRepository,
+    private router: Router,
+  ) {
+    this.logoutUseCase = new LogoutUseCase(
+      authenticatedUserRepository,
+      authRepository,
+    );
+  }
 
   intercept(
     request: HttpRequest<unknown>,
@@ -27,7 +45,13 @@ export class ApiInterceptor implements HttpInterceptor {
   }
 
   throwMessageError(err: HttpErrorResponse): Observable<never> {
+    if (err.status === 401) {
+      this.logoutUseCase.execute().subscribe((response) => {
+        this.router.navigate(['/login']);
+      });
+    }
     let error = err.error;
+
     let message = 'internal server error';
 
     if (error && typeof error === 'string') {
