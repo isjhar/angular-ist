053bad42db0b7c07c2568f503df508b4b3af4195
Isjhar Kautsar <isjhar@gmail.com>
2026-04-03 19:02:14 +0800

    add global mask config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -27,7 +27,10 @@ export const appConfig: ApplicationConfig = {
     provideRepositories(),
     provideDateAdapter(),
     provideCharts(withDefaultRegisterables()),
-    provideEnvironmentNgxMask(),
+    provideEnvironmentNgxMask({
+      thousandSeparator: '.',
+      decimalMarker: ',',
+    }),
     authInterceptorProviders,
     apiInterceptorProviders,
     provideRouter(routes),
