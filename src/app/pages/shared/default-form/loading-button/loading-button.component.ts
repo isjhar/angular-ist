73c7d8693120dce10518d8c92b4df85126427146
Isjhar Kautsar <isjhar@gmail.com>
2026-09-07 16:13:08 +0800

    update loading button

--- src/app/pages/shared/default-form/loading-button/loading-button.component.ts
+++ src/app/pages/shared/default-form/loading-button/loading-button.component.ts
@@ -1,19 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatButtonAppearance } from '@angular/material/button';
 import { NgStyle } from '@angular/common';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatIcon } from '@angular/material/icon';
+
+export type ButtonAppearance = 'filled' | 'outlined' | 'ghost';
 
 @Component({
   selector: 'app-loading-button',
   templateUrl: './loading-button.component.html',
   styleUrls: ['./loading-button.component.scss'],
-  imports: [MatButton, NgStyle, MatProgressSpinner],
+  imports: [MatButton, NgStyle, MatProgressSpinner, MatIcon],
   standalone: true,
 })
 export class LoadingButtonComponent implements OnInit {
   @Input() disabled: boolean = false;
   @Input() isLoading: boolean = false;
   @Input() width: number | string = 100;
+  @Input() appearance: MatButtonAppearance = 'filled';
+  @Input() name?: string;
+  @Input() value?: string;
+  @Input() icon?: string;
 
   constructor() {}
 
